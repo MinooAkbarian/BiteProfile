93d515c44843fe7b8cf1687d83795cf61c6d1d77
Minoo Akbarian <minooak@gmail.com>
2012-05-08 14:46:34 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BiteProfile::Application.config.secret_token = 'e063763879c1577fd2140d09f5a58a9c560bd6858d489c8441e2c017d4fad2785ce91edb70d9c74c8da0bc667e5bad9fe0f1abb21b2140764a56193fc64112af'
