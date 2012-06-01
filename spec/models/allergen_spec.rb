d03642b4932d80a7c319a0ea544f6ee1d6c9fcf9
Minoo Akbarian <minooak@gmail.com>
2012-06-01 13:40:16 -0700

    added allergen class and created specs for allergy and allergen

--- spec/models/allergen_spec.rb
+++ spec/models/allergen_spec.rb
@@ -2,13 +2,14 @@ require 'spec_helper'
 
 describe Allergen do
   before do
+  @allergen1 = Allergen.create(:name => 'nuts')
+  @allergen2 = Allergen.create(:name => 'milk')
    @user = User.create(:name => 'minoo', :email => 'biteprofile@gmail.com', :password => 'letstest')
    @product = Product.create(:name => 'bread', :user_id => @user.id)
    @allergy1 = Allergy.create(:allergable_id => @user.id, :allergable_type => @user.class.name)
    @allergy2 = Allergy.create(:allergable_id => @product.id, :allergable_type => @product.class.name)
-   @allergen1 = Allergen.create(:name => 'nuts', :allergen_id => @allergy1.id)
-   @allergen2 = Allergen.create(:name => 'milk', :allergen_id => @allergy2.id)
- end
+
+  end
 
 #relationships
 
