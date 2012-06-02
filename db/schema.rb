e38e997fa05bceac0ab8866d3c9944e929ded0b9
Minoo Akbarian <minooak@gmail.com>
2012-06-01 17:05:34 -0700

    regenerated allergen

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120522214434) do
+ActiveRecord::Schema.define(:version => 20120602000031) do
+
+  create_table "allergens", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "allergies", :force => true do |t|
     t.integer  "allergable_id"
